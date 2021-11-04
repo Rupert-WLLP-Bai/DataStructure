b201f0600ab648449869018a5e91f0baa4f4e668
Rupert-WLLP-Bai <2052526@tongji.edu.cn>
2021-11-04 23:17:35 +0800

    Q2 Version 1.0.3

--- Q02/src/main.cpp
+++ Q02/src/main.cpp
@@ -1,19 +1,23 @@
 /*
 @name:		  main.cpp
 @description: 主函数
-@version:     v1.0.0
+@version:     v1.0.1
 @author:      Laobai
-@time:        2021年10月22日11:06:54
+@time:        2021年11月4日23:15:59
 */
 
 /*
 update notes:
-
 v1.0.0 2021年10月22日11:06:54
+v1.0.1 2021年11月4日23:15:59
 */
 
 #include "joseph.h"
 int main() {
+#ifdef _WIN32
+    system("chcp 65001");
+#endif
+
     Solution();
     return 0;
 }
