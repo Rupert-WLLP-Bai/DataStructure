c7a5b7f1ae226712b92b19ce56ccd3104d8f19d7
Rupert-WLLP-Bai <2052526@tongji.edu.cn>
2021-11-08 19:10:38 +0800

    update

---
+++ Q09/include/solution.h
@@ -0,0 +1,14 @@
+/**
+ * @file solution.h
+ * @brief 主函数包含的头文件
+ * @author Laobai (CWLLP1230@gmail.com)
+ * @version 1.0
+ * @date 2021-11-08
+ * 
+ * @copyright Copyright (c) 2021  Laobai
+ * 
+ */
+
+#include<iostream>
+
+void solution();
---
+++ Q09/src/main.cpp
@@ -0,0 +1,11 @@
+#include "solution.h"
+using namespace std;
+
+//主函数
+int main() {
+#ifdef _WIN32
+    system("chcp 65001");
+#endif
+    solution();
+    return 0;
+}
