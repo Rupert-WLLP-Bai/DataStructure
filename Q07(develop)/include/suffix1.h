b157ceaeaf30be75e241fdd37464a896d2aa63c3
Rupert-WLLP-Bai <2052526@tongji.edu.cn>
2021-12-04 17:30:29 +0800

    大致写了一下思路 理清思路后进行实现

---
+++ Q07(develop)/include/suffix1.h
@@ -0,0 +1,14 @@
+/*************************************************************************
+	> File Name: include/suffix1.h
+	> Author: Laobai
+	> Mail: 1762161822@qq.com 
+	> Created Time: Sat 04 Dec 2021 04:29:31 PM CST
+ ************************************************************************/
+
+
+#include<iostream>
+#include<stack>
+#include<string>
+using std::string;
+
+
